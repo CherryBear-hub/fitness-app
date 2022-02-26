10191d9f7ffa8bd0665d44f6a0b3d6607b89944a
Haroldas Valciukas <haroldas.valciukas@gmail.com>
2022-02-26 12:12:44 +0200

    health module routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Store } from 'store';
 import { AuthModule } from '../auth/auth.module';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppNavComponent } from './components/app-nav/app-nav.component';
+import {HealthModule} from "../health/health.module";
 
 export const ROUTES: Routes = [];
 
@@ -16,6 +17,7 @@ export const ROUTES: Routes = [];
     BrowserModule,
     RouterModule.forRoot(ROUTES),
     AuthModule,
+    HealthModule
   ],
   providers: [Store],
   bootstrap: [AppComponent],
