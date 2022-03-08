9561ec119d6ca19b7cfb13deddcb4132fb2a4610
Haroldas Valciukas <haroldas.valciukas@gmail.com>
2022-03-08 21:23:33 +0200

    workout conditional form controls

--- src/health/workouts/components/workout-form/workout-form.component.ts
+++ src/health/workouts/components/workout-form/workout-form.component.ts
@@ -1,6 +1,18 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-import {instanceOfWorkout, Workout, WorkoutType} from '../../../../utils/types';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import {
+  instanceOfWorkout,
+  Workout,
+  WorkoutType,
+} from '../../../../utils/types';
 
 @Component({
   selector: 'fit-workout-form',
@@ -8,7 +20,7 @@ import {instanceOfWorkout, Workout, WorkoutType} from '../../../../utils/types';
   templateUrl: './workout-form.component.html',
   styleUrls: ['./workout-form.component.scss'],
 })
-export class WorkoutFormComponent implements OnChanges{
+export class WorkoutFormComponent implements OnChanges {
   @Input() workout: Workout | {} = {};
 
   @Output() create = new EventEmitter<Workout>();
@@ -16,37 +28,67 @@ export class WorkoutFormComponent implements OnChanges{
   @Output() remove = new EventEmitter<void>();
 
   workoutExists = false;
+  workoutType = WorkoutType;
+  placeholder = 'Benchpress';
 
   form = this.formBuilder.group({
     name: ['', Validators.required],
-    type: WorkoutType.Strength
+    type: WorkoutType.Strength,
+    strength: this.formBuilder.group({
+      reps: 0,
+      sets: 0,
+      weight: 0,
+    }),
+    endurance: this.formBuilder.group({
+      distance: 0,
+      duration: 0,
+    }),
   });
+
+  onTypeChange(value: WorkoutType) {
+    this.placeholder =
+      value === this.workoutType.Strength ? 'Benchpress' : 'Treadmill';
+  }
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnChanges(changes: SimpleChanges) {
-    if(instanceOfWorkout(this.workout)){
+    if (instanceOfWorkout(this.workout)) {
       this.workoutExists = true;
       this.form.patchValue(this.workout);
-    }
-    else{
+    } else {
       this.workoutExists = false;
     }
   }
 
   createWorkout() {
     if (this.form.valid) {
+      this.clearUnusedType();
       this.create.emit(this.form.value);
     }
   }
 
   updateWorkout() {
     if (this.form.valid) {
+      this.clearUnusedType();
       this.update.emit(this.form.value);
     }
   }
 
   removeWorkout() {
     this.remove.emit();
+  }
+
+  clearUnusedType(): void {
+    this.form.get('type')?.value == this.workoutType.Strength
+      ? this.form.get('endurance')?.reset({
+        distance: 0,
+        duration: 0,
+      })
+      : this.form.get('strength')?.reset({
+        reps: 0,
+        sets: 0,
+        weight: 0,
+      });
   }
 }
